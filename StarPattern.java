2cd2d3985a1870a5add1f92b59eb8a3fdb95d4df
Saurabh Alhat <120566574+SaurabhAlhat85@users.noreply.github.com>
2022-12-15 19:04:02 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,8 @@
+public class StarPattern {
+    public static void main(String args[]) {
+        System.out.println("* * * *");  
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
