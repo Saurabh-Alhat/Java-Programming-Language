f98ca0871d0b6da1f450cf5ac8aa681a47d65ddd
Saurabh Alhat <120566574+SaurabhAlhat85@users.noreply.github.com>
2022-12-15 18:59:37 +0530

    Add files via upload

---
+++ OutputBasics.java
@@ -0,0 +1,7 @@
+public class OutputBasics {
+    public static void main(String args[]) {
+        System.out.print("Hello World!\n Hello \n World! "); //Line ka space 
+        System.out.println("Hello World!"); //Line ka space
+        System.out.println("Hello World!"); //Line ka space
+    }
+}
