89389b4370ec509e673ec16d5168016898ddf086
Saurabh Alhat <120566574+SaurabhAlhat85@users.noreply.github.com>
2022-12-16 17:15:47 +0530

    Add files via upload

---
+++ VariableBasics.java
@@ -0,0 +1,15 @@
+public class VariableBasics {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 20;
+        
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Tony Stark";
+        System.out.println(name);
+
+        a = b;
+        System.out.println(a);
+    }
+}
