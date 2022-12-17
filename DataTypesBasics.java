94223f9b76063f55dcac75fcf82e5fcd1d726b2f
Saurabh Alhat <120566574+SaurabhAlhat85@users.noreply.github.com>
2022-12-17 17:47:13 +0530

    Add files via upload

---
+++ DataTypesBasics.java
@@ -0,0 +1,24 @@
+public class DataTypesBasics {
+    public static void main(String args[]) {
+
+       byte b = 5;
+       System.out.println(b);
+
+       short n = 240;
+       System.out.println(n);
+
+       char ch = 'a';
+       System.out.println(ch);
+
+       boolean var = false;
+       System.out.println(var);
+
+       int number = 25;
+       System.out.println(number);
+    //    long
+       
+       float price = 10.5;
+    //    double
+
+    }
+}
