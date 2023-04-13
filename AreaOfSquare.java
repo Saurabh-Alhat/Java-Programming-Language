d8829c367f36b100046f331992106fd670cee9de
Saurabh Alhat <120566574+Saurabh-Alhat@users.noreply.github.com>
2023-04-13 21:01:27 +0530

    Add files via upload

---
+++ AreaOfSquare.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class AreaOfSquare {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int side = sc.nextInt();
+
+        int area = (side * side);
+
+        System.out.println("The area of square is : " + area);
+    }
+}
