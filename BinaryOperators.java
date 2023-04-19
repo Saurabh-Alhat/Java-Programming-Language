f54221b4d2a61b36e4855bc8234230caaa9b4319
Saurabh Alhat <120566574+Saurabh-Alhat@users.noreply.github.com>
2023-04-19 22:52:18 +0530

    Add files via upload

---
+++ BinaryOperators.java
@@ -0,0 +1,12 @@
+public class BinaryOperators {
+    public static void main(String args[]) {
+        int A = 10;
+        int B = 5;
+
+        System.out.println("Addition = " + (A + B));
+        System.out.println("Subtraction = " + (A - B));
+        System.out.println("Multiplication = " + (A * B));
+        System.out.println("Division = " + (A / B));
+        System.out.println("Modulo (Remainder) = " + (A % B));
+    }
+}
