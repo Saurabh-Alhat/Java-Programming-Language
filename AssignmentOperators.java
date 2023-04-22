f122ca7ecb559421fc8c4fad6301b605e838ce4c
Saurabh Alhat <120566574+Saurabh-Alhat@users.noreply.github.com>
2023-04-22 23:47:20 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,32 @@
+public class AssignmentOperators {
+    public static void main(String args[]) {
+        System.out.println("\n(=) Operator");
+        int A = 10;
+        System.out.println("A = " + A);
+
+        System.out.println("\n(+=) Operator");
+        int B = 5;
+        B += 5;  // This means, B = B + 5
+        System.out.println("B = " + B);
+
+        System.out.println("\n(-=) Operator");
+        int C = 5;
+        C -= 5;  // This means, C = C - 5
+        System.out.println("C = " + C);
+
+        System.out.println("\n(*=) Operator");
+        int D = 5;
+        D *= 5;  // This means, D = D * 5
+        System.out.println("D = " + D);
+
+        System.out.println("\n(/=) Operator");
+        int E = 5;
+        E /= 5;  // This means, E = E / 5
+        System.out.println("E = " + E);
+
+        System.out.println("\n(%=) Operator");
+        int F = 5;
+        F %= 5;  // This means, F = F % 5
+        System.out.println("F = " + F);
+    }
+}
